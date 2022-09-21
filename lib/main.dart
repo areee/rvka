6368180fd2eaae81dadbdfe2bcead37ffa9a2180
areee <10089872+areee@users.noreply.github.com>
2022-09-21 18:00:33 +0300

    Text file addition that shows file path when chosen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-
 import 'package:file_selector/file_selector.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,6 +13,11 @@ class MyApp extends StatelessWidget {
       title: 'RVKA',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.green,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
         primarySwatch: Colors.green,
       ),
       home: const MyHomePage(title: 'RVKA - Ruokaverkkokaupan apuri'),
@@ -33,8 +35,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _contentController = TextEditingController();
+  // final TextEditingController _nameController = TextEditingController();
+  // final TextEditingController _contentController = TextEditingController();
+
+  String _textFilePath = "";
 
   Future<void> _openTextFile(BuildContext context) async {
     final XTypeGroup typeGroup = XTypeGroup(
@@ -46,28 +50,28 @@ class _MyHomePageState extends State<MyHomePage> {
 
     if (file == null) return;
 
-    final String fileName = file.name;
-    final String fileContent = await file.readAsString();
+    final String filePath = file.path;
 
-    print(fileName);
-    print(fileContent);
+    setState(() {
+      _textFilePath = filePath;
+    });
   }
 
-  Future<void> _saveTextFile() async {
-    final String fileName = _nameController.text;
+  // Future<void> _saveTextFile() async {
+  //   final String fileName = _nameController.text;
 
-    final String? path = await getSavePath(suggestedName: fileName);
+  //   final String? path = await getSavePath(suggestedName: fileName);
 
-    if (path == null) return;
+  //   if (path == null) return;
 
-    final String text = _contentController.text;
-    final Uint8List fileData = Uint8List.fromList(utf8.encode(text));
-    const String fileMimeType = 'text/plain';
-    final XFile textFile =
-        XFile.fromData(fileData, mimeType: fileMimeType, name: fileName);
+  //   final String text = _contentController.text;
+  //   final Uint8List fileData = Uint8List.fromList(utf8.encode(text));
+  //   const String fileMimeType = 'text/plain';
+  //   final XFile textFile =
+  //       XFile.fromData(fileData, mimeType: fileMimeType, name: fileName);
 
-    await textFile.saveTo(path);
-  }
+  //   await textFile.saveTo(path);
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -80,36 +84,47 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ElevatedButton(
-                onPressed: () => _openTextFile(context),
-                child: const Text('Select a file')),
-            const SizedBox(height: 10),
-            SizedBox(
-              width: 300,
-              child: TextField(
-                minLines: 1,
-                maxLines: 12,
-                controller: _nameController,
-                decoration: const InputDecoration(
-                  hintText: '(Optional) Suggest File Name',
-                ),
-              ),
+            const Text(
+              'Select a text file',
+              style: TextStyle(fontSize: 20),
             ),
-            SizedBox(
-              width: 300,
-              child: TextField(
-                minLines: 1,
-                maxLines: 12,
-                controller: _contentController,
-                decoration: const InputDecoration(
-                  hintText: 'Enter File Contents',
-                ),
-              ),
+            TextAndButton(
+              text: _textFilePath,
+              buttonText: 'Open text file',
+              onPressed: () => _openTextFile(context),
             ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-                onPressed: () => _saveTextFile(),
-                child: const Text('Save a file')),
+            // Text('Selected text file: $_textFilePath'),
+            // ElevatedButton(
+            //     onPressed: () => _openTextFile(context),
+            //     child: const Text('Select a file')),
+            // =====
+            // const SizedBox(height: 10),
+            // SizedBox(
+            //   width: 300,
+            //   child: TextField(
+            //     minLines: 1,
+            //     maxLines: 12,
+            //     controller: _nameController,
+            //     decoration: const InputDecoration(
+            //       hintText: '(Optional) Suggest File Name',
+            //     ),
+            //   ),
+            // ),
+            // SizedBox(
+            //   width: 300,
+            //   child: TextField(
+            //     minLines: 1,
+            //     maxLines: 12,
+            //     controller: _contentController,
+            //     decoration: const InputDecoration(
+            //       hintText: 'Enter File Contents',
+            //     ),
+            //   ),
+            // ),
+            // const SizedBox(height: 10),
+            // ElevatedButton(
+            //     onPressed: () => _saveTextFile(),
+            //     child: const Text('Save a file')),
           ],
         ),
       ),
@@ -119,4 +134,31 @@ class _MyHomePageState extends State<MyHomePage> {
   // void openFile(PlatformFile file) {
   //   OpenFile(file.path!);
   // }
+}
+
+class TextAndButton extends StatelessWidget {
+  const TextAndButton(
+      {super.key,
+      required this.text,
+      required this.buttonText,
+      required this.onPressed});
+
+  final String text;
+  final String buttonText;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Flexible(
+          child: Text(text),
+        ),
+        ElevatedButton(
+          onPressed: onPressed,
+          child: Text(buttonText),
+        ),
+      ],
+    );
+  }
 }
